053daaf4bceba87e3f26d780180507590284e1a5
Khaled Elgendy <khaled.elgendy@trivago.com>
2023-04-04 15:33:23 +0200

    Fresnel implementation

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,24 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import fresnel from '../lib/fresnel';
+
+const { createMediaStyle } = fresnel;
+
+const mediaStyles = createMediaStyle();
 
 export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+    return (
+        <Html lang="en">
+            <Head>
+                <style
+                    dangerouslySetInnerHTML={{
+                        __html: mediaStyles.replace(/\n/g, ''),
+                    }}
+                />
+            </Head>
+            <body>
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    )
 }
